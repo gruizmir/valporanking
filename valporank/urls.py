49ca6d8e5f55aecc8cbe440c0160905b30cfbb3f
Gabriel Ruiz M <gabriel.ruiz.miranda@gmail.com>
2013-12-01 22:39:13 -0300

    Agregado codigo para subir, votar y obtener lista de imagenes

--- valporank/urls.py
+++ valporank/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,5 +10,8 @@ urlpatterns = patterns('',
     url(r'^rank/', 'main.views.rank'),
     url(r'^best/', 'main.views.best'),
     url(r'^hipster/', 'main.views.worst'),
+    url(r'^sendimage/', 'main.views.sendImage'),
+    url(r'^voteimage/', 'main.views.voteImage'),
+    url(r'^getimages', 'main.views.getImages'),
     url(r'^admin/', include(admin.site.urls)),
-)
+)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
